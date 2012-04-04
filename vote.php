412f7bf8299e57d2af83ee4623dee2254d819842
melchor629 <melchor9000@gmail.com>
2012-04-04 22:13:49 +0300

    Quitando una palabra que sobra...

--- vote.php
+++ vote.php
@@ -226,14 +226,14 @@ _gaq.push(['_trackPageLoadTime']);
 										$voteable = 0;
 										
 										if($timp['ore'] > 0)
-											if($timp['ore'] > 1) $in_time = 'in '.$timp['ore'].''.$Vote['Vote24'].'';
-											else $in_time = 'in '.$timp['ore'].''.$Vote['Vote34'].'';
+											if($timp['ore'] > 1) $in_time = ''.$timp['ore'].''.$Vote['Vote24'].'';
+											else $in_time = ''.$timp['ore'].''.$Vote['Vote34'].'';
 										else if($timp['min'] > 0)
-											if($timp['min'] > 1) $in_time = 'in '.$timp['min'].''.$Vote['Vote25'].'';
-											else $in_time = 'in '.$timp['min'].''.$Vote['Vote35'].'';
+											if($timp['min'] > 1) $in_time = ''.$timp['min'].''.$Vote['Vote25'].'';
+											else $in_time = ''.$timp['min'].''.$Vote['Vote35'].'';
 										else if($timp['sec'] > 0)
-											if($timp['sec'] > 1) $in_time = 'in '.$timp['sec'].''.$Vote['Vote26'].'';
-											else $in_time = 'in '.$timp['sec'].''.$Vote['Vote27'].'';
+											if($timp['sec'] > 1) $in_time = ''.$timp['sec'].''.$Vote['Vote26'].'';
+											else $in_time = ''.$timp['sec'].''.$Vote['Vote27'].'';
 										else $voteable = 1;
 									}
 									
